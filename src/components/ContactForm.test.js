6492e1723768670563071e0fa0d01ee8595c3990
Joe Witzke <joewitzke@joes-mbp.lan>
2021-05-06 16:00:11 -0600

    Some code added for first name error and email error tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -22,7 +22,19 @@ test('renders the contact form header', ()=> {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
+    // Arrange: Render out component.
+    render(<ContactForm/>);
     
+
+    // Act: Select name input field and enter first name less than 5 characters.
+    const nameInput = screen.getByLabelText(/firstname/i);
+    userEvent.type(nameInput, "Joe");
+
+    //Assert: there will be an error message displayed.
+    const errorDisplay = await screen.findByText("Joe");
+    // expect(nameInput).toBeInTheDocument();
+    // expect(nameInput).toHaveValue("Joe")
+    expect(errorDisplay).toEqual("firstName must have at least 5 characters.");
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
@@ -34,7 +46,15 @@ test('renders ONE error message if user enters a valid first name and last name 
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+    //Arrange
+    // render(<ContactForm/>);
+    // const emailInput = screen.getByLabelText("email");
+    //Act
+    // userEvent.type(emailInput, "test");
+    //Assert
+    // expect(emailInput).toHaveValue("test");
+    // expect(emailInput).toBeInTheDocument();
+    // expect(screen.queryByTestId("error").textContent).toEqual("email must be a valid email address");
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
